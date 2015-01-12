91397655449f10c2c7cd799ab988e68c2a4d2a68
oroce <robert@oroszi.net>
2015-01-12 21:57:42 +0100

    i should write more tests

--- spec/outputs/s3_spec.rb
+++ spec/outputs/s3_spec.rb
@@ -1,6 +1,27 @@
 # encoding: utf-8
 require "logstash/devutils/rspec/spec_helper"
-require 'logstash/outputs/s3'
+require "logstash/outputs/s3"
 
 describe LogStash::Outputs::S3 do
+  it "should register" do
+    output = LogStash::Plugin.lookup("output", "s3").new("temp_directory" => "./")
+
+    # register will try to load jars and raise if it cannot find jars
+    expect {output.register}.to_not raise_error
+  end
+#  describe 'AWS' do
+#  end
+
+  describe "sprintf" do
+
+    before :each do
+      @s3 = LogStash::Outputs::S3.new("temp_directory" => "./")
+    end
+
+    it "should format %Y" do
+      @s3.sprintf("%{YEAR}").should eq(Time.now.year)
+    end
+
+  end
+  #  it "should gzip"
 end
